0d9324a433226f1022acf5ff7ae74a9b4c246f2c
Pushpinder Pal Singh <pushpinder@tutamail.com>
2020-06-23 09:03:44 +0530

    TEST CI

--- SpaceXDummyTest/SpaceXDummyTest.swift
+++ SpaceXDummyTest/SpaceXDummyTest.swift
@@ -23,11 +23,6 @@ class SpaceXDummyTest: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
 
 }
