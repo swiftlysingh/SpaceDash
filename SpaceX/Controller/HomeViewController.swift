6df499c0605e3b2479571f1cced2643e98d08725
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-07 23:15:04 +0530

    Update  Upcoming Launch Panel Constraintssdjvk

--- SpaceX/Controller/HomeViewController.swift
+++ SpaceX/Controller/HomeViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var upcomingLaunchHeight: NSLayoutConstraint!
+    @IBOutlet weak var upcomingPanel: NSLayoutConstraint!
+    @IBOutlet var panelConstraints: [NSLayoutConstraint]!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        upcomingLaunchHeight.constant = UIScreen.main.bounds.height*0.55
+        if(UIScreen.main.bounds.height<896){
+            updateUIConstraints()
+        }
+
+    }
+    func updateUIConstraints(){
+                upcomingPanel.constant = UIScreen.main.bounds.height*0.045
+                
+                for panels in panelConstraints{
+                    panels.constant = UIScreen.main.bounds.height*0.03
+                }
     }
 }
