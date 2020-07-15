f0ca285d3da3f973ffe135044fadeb92cbd399a2
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-15 21:00:24 +0530

    Add Network Manager Struct

---
+++ NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  SpaceX
+//
+//  Created by Pushpinder Pal Singh on 15/07/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkManager{
+    
+}
