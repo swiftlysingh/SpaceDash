81f70ad2168b70297a54f0554c57dce220540df6
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-26 15:52:46 +0530

    Add UI Test for checking Constant file

---
+++ SpaceDashConstantsCheck/SpaceDashConstantsCheck.swift
@@ -0,0 +1,32 @@
+//
+//  SpaceDashConstantsCheck.swift
+//  SpaceDashConstantsCheck
+//
+//  Created by Pushpinder Pal Singh on 26/07/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import XCTest
+
+@testable import SpaceDash
+
+class SpaceDashConstantsCheck: XCTestCase {
+    
+    var sut = SpaceDash.Constants.self
+    
+    override func setUpWithError() throws {
+        super.setUp()
+    }
+
+    override func tearDownWithError() throws {
+        super.tearDown()
+    }
+
+    func testConstant() throws {
+        XCTAssertEqual("https://api.spacexdata.com/v3/", sut.Networking.baseURL)
+        XCTAssertEqual("launches/Upcoming", sut.Networking.upcomingLaunchURL)
+        XCTAssertEqual("Launch Site Not Declared", sut.defaultArgs.launchSite)
+        XCTAssertEqual("Not Available", sut.defaultArgs.noData)
+    }
+
+}
