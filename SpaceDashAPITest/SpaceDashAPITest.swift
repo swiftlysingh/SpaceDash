05783d7f56dddbf18eed024d11abf2c9a3faabbb
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-07-26 16:25:51 +0530

    Add UI Test for API check

---
+++ SpaceDashAPITest/SpaceDashAPITest.swift
@@ -0,0 +1,46 @@
+//
+//  SpaceDashAPITest.swift
+//  SpaceDashAPITest
+//
+//  Created by Pushpinder Pal Singh on 26/07/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import XCTest
+
+@testable import SpaceDash
+
+class SpaceDashAPITest: XCTestCase {
+    
+    var sut: URLSession!
+    
+    override func setUpWithError() throws {
+        super.setUp()
+        sut = URLSession(configuration: .default)
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testAPICall() throws {
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        let dataTask = sut.dataTask(with:URL(string: Constants.Networking.baseURL)!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+        }
+        dataTask.resume()
+        wait(for: [promise], timeout: 5)
+        
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
+    }
+}
+
+
