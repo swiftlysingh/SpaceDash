c8f21b71f8534884b59bb6d1e4cf9daea45670be
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-08-03 22:45:03 +0530

    Cell Design Complete

---
+++ SpaceDash/View/DetailsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsTableViewCell.swift
+//  SpaceDash
+//
+//  Created by Pushpinder Pal Singh on 03/08/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var details: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
