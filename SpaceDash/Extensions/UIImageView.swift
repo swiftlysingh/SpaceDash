8c7d2a6d18e4592ce81679717c3f895a229ad72b
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-08-08 00:29:36 +0530

    Extension UIImage

---
+++ SpaceDash/Extensions/UIImageView.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage.swift
+//  SpaceDash
+//
+//  Created by Pushpinder Pal Singh on 08/08/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+   func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+      URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+   }
+   func downloadImage(from url: URL) {
+      getData(from: url) {
+         data, response, error in
+         guard let data = data, error == nil else {
+            return
+         }
+         DispatchQueue.main.async() {
+            self.image = UIImage(data: data)
+         }
+      }
+   }
+}
