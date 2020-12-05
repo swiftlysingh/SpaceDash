f6f223ad0549681fb30fdb49146f489274bc29b0
Akhigbe benjamin <simplybenjamin2.0@gmail.com>
2020-12-06 00:46:35 +0100

    Style: Add News and Article Storyboard
    because: user not not able to read current trends happening in the world of space exploration
    
    therefore, it is important to have a view that shows the list of recent news and article relating to space exploration.

---
+++ SpaceDash/Controller/ViewControllers/ArticleListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleListViewController.swift
+//  SpaceDash
+//
+//  Created by akhigbe benjamin on 06/12/2020.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+
+class ArticleListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SpaceDash/Controller/ViewControllers/NewsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsListViewController.swift
+//  SpaceDash
+//
+//  Created by akhigbe benjamin on 06/12/2020.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+
+class NewsListViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
